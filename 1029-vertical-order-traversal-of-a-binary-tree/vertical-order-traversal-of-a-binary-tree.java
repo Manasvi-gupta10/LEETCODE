9aaa9e61491a40f97a8008b0ed910649101843ac
Manasvi gupta <manasvi10amit@gmail.com>
2025-08-28 12:03:17 +0530

    Time: 3 ms (81.76%) | Memory: 43 MB (13.54%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,49 @@
+class Solution {
+    class VP{
+        TreeNode node;
+        int l;//row
+        int v;//col
+        public VP(TreeNode node,int l,int v){
+            this.l=l;
+            this.v=v;
+            this.node=node;
+        }
+    }
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        Queue<VP> q=new LinkedList<>();
+        q.add(new VP(root,0,0));
+        TreeMap<Integer,List<VP>> mp=new TreeMap<>();
+        while(!q.isEmpty()){
+            VP vp=q.poll();
+            if(!mp.containsKey(vp.v)){
+                mp.put(vp.v,new ArrayList<>());
+            }
+            mp.get(vp.v).add(vp);
+            if(vp.node.left!=null){
+                q.add(new VP(vp.node.left,vp.l+1,vp.v-1));
+            }
+            if(vp.node.right!=null){
+                q.add(new VP(vp.node.right,vp.l+1,vp.v+1));
+            }
+        }
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int key:mp.keySet()){
+            List<VP> ll=mp.get(key);
+            Collections.sort(ll,new Comparator<VP>(){
+                @Override
+                public int compare(VP o1,VP o2){
+                    if(o1.l==o2.l){
+                        return o1.node.val-o2.node.val;
+                    }
+                    return 0;
+                }
+            });
+            List<Integer> ls=new ArrayList<>();
+            for(VP v:ll){
+                ls.add(v.node.val);
+            }
+            ans.add(ls);
+        }
+        return ans;
+    }
+}
