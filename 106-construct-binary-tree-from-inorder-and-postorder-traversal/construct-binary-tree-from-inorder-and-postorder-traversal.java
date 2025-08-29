4836a035877e4de50466b525523cba0f1d849305
Manasvi gupta <manasvi10amit@gmail.com>
2025-08-29 14:04:21 +0530

    Time: 3 ms (27.51%) | Memory: 44.5 MB (52.76%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,23 +15,24 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return build(postorder,inorder,0,inorder.length-1,0,postorder.length-1);
+        TreeNode root=tree(inorder,postorder,0,inorder.length-1,0,postorder.length-1);
+        return root;
     }
-    public TreeNode build(int[] post,int[] in,int ilo,int ihi,int plo,int phi){
+    public TreeNode tree(int[] in,int[] post,int ilo,int ihi,int plo,int phi){
         if(ilo>ihi||plo>phi){
             return null;
         }
+        TreeNode nn=new TreeNode(post[phi]);
+        int idx=find(in,ilo,ihi,post[phi]);
+        int c=idx-ilo-1;
+        nn.left=tree(in,post,ilo,idx-1,plo,plo+c);
+        nn.right=tree(in,post,idx+1,ihi,plo+c+1,phi-1);
+        return nn;
 
-        TreeNode root=new TreeNode(post[phi]);
-        int idx=search(in,ilo,ihi,post[phi]);
-        int c=idx-ilo;
-        root.left=build(post,in,ilo,idx-1,plo,plo+c-1);
-        root.right=build(post,in,idx+1,ihi,plo+c,phi-1);
-        return root;
     }
-    public int search(int[] in,int ilo,int ihi,int elm){
+    public int find(int[] in,int ilo,int ihi,int e){
         for(int i=ilo;i<=ihi;i++){
-            if(in[i]==elm){
+            if(in[i]==e){
                 return i;
             }
         }
