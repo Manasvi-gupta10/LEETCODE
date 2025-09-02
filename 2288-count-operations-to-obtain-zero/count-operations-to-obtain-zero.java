10bb517fd8e90cff0d560145a9ea08973359d777
Manasvi gupta <manasvi10amit@gmail.com>
2025-09-02 22:53:14 +0530

    Time: 1 ms (91.81%) | Memory: 40.7 MB (49.32%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int step=0;
+        while(num1!=0||num2!=0){
+            if(num1==0){
+                break;
+            }
+            if(num2==0){
+                break;
+            }
+            if(num1>num2){
+                num1-=num2;
+                step++;
+            }else{
+                num2-=num1;
+                step++;
+            }
+        }
+        return step;
+    }
+}
