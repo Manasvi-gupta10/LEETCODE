598b5b8bf92bc18eedf7a9b0c878ae27e82ce700
Manasvi gupta <manasvi10amit@gmail.com>
2025-09-28 23:36:50 +0530

    Time: 7 ms (17.34%) | Memory: 44.5 MB (12.21%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,9 +1,21 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n=piles.length;
+        int[][] dp=new int[n][n];
+
+        int maxa=maxadv(piles,0,n-1,dp);
+        return maxa>0;
     }
-    public int maxc(int [] piles,boolean isalice,int i,int j){
-        return 0;
+    public int maxadv(int[] p,int i,int j,int[][] dp){
+        if(i==j){
+            return p[i];
+        }
+        if(dp[i][j]!=0){
+            return dp[i][j];
+        }
+        int l=p[i]-maxadv(p,i+1,j,dp);
+        int r=p[j]-maxadv(p,i,j-1,dp);
+
+        return dp[i][j]=Math.max(l,r);
     }
-        
-    }
+}
