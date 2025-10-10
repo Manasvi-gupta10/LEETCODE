97c23b1e334e5389699ec387b5ef08d221df1fa5
Manasvi gupta <manasvi10amit@gmail.com>
2025-10-10 14:54:36 +0530

    Time: 2 ms (94.12%) | Memory: 47 MB (21.74%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -7,7 +7,7 @@ class Solution {
         }
         dp[1]=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i>>1]+(i&1);
+            dp[i]=dp[i/2]+(i&1);
         }
         return dp;
     }
