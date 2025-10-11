3c164a6f11fe04a233532b624bac16a383ad46d4
Manasvi gupta <manasvi10amit@gmail.com>
2025-10-11 23:10:50 +0530

    Time: 6 ms (17.31%) | Memory: 44.5 MB (11.38%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,21 +1,27 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
         int n=piles.length;
+        int tot=0;
         int[][] dp=new int[n][n];
-
-        int maxa=maxadv(piles,0,n-1,dp);
-        return maxa>0;
+        for(int i=0;i<n;i++){
+            tot+=piles[i];
+        }
+        int maxp1=maxsc(piles,0,n-1,dp);
+        int maxp2=tot-maxp1;
+        if(maxp1>maxp2){
+            return true;
+        }
+        return false;
     }
-    public int maxadv(int[] p,int i,int j,int[][] dp){
-        if(i==j){
-            return p[i];
+    public int maxsc(int[] ar,int i,int j,int[][] dp){
+        if(i>j){
+            return 0;
         }
         if(dp[i][j]!=0){
             return dp[i][j];
         }
-        int l=p[i]-maxadv(p,i+1,j,dp);
-        int r=p[j]-maxadv(p,i,j-1,dp);
-
-        return dp[i][j]=Math.max(l,r);
+        int f=ar[i]+Math.min(maxsc(ar,i+1,j-1,dp),maxsc(ar,i+2,j,dp));
+        int s=ar[j]+Math.min(maxsc(ar,i,j-2,dp),maxsc(ar,i+1,j-1,dp));
+        return dp[i][j]=Math.max(f,s);
     }
 }
