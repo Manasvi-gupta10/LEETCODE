9fdb258f5b62cbd7f59681c33cdeaf1464118e71
Manasvi gupta <manasvi10amit@gmail.com>
2025-12-02 20:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (38.14%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -20,9 +20,8 @@ class Solution {
         }
         if(root.val<val){
             root.right=insertIntoBST(root.right,val);
-        }
-        else if(root.val>val){
-            root.left=insertIntoBST(root.left,val);
+        }else{
+            root.left= insertIntoBST(root.left,val);
         }
         return root;
     }
