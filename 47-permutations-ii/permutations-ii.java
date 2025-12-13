339d71f413a3e6b89c5b9207938607b50f6507ff
Manasvi gupta <manasvi10amit@gmail.com>
2025-12-13 16:33:01 +0530

    Time: 4 ms (36.85%) | Memory: 46.5 MB (54.95%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -2,28 +2,48 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> ls=new ArrayList<>();
-        List<Integer> idx=new ArrayList<>();
-        per2(nums,ans,ls,idx);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        per2(nums,ans,ls,map);
         return ans;
 
         
     }
-    public void per2(int[] nums,List<List<Integer>> ans,List<Integer> ls,List<Integer> idx){
-        if(idx.size()==nums.length){
-            if(!ans.contains(ls)){
-                ans.add(new ArrayList<>(ls));
-            }
-            return;
+    public void per2(int[] nums,List<List<Integer>> ans,List<Integer> ls,HashMap<Integer,Integer> map){
+        if(ls.size()==nums.length){
+            ans.add(new ArrayList<>(ls));
         }
-        for(int i=0;i<nums.length;i++){
-            if(!idx.contains(i)){
-                idx.add(i);
-                ls.add(nums[i]);
-                per2(nums,ans,ls,idx);
+        for(int k:map.keySet()){
+            if(map.get(k)>0){
+                ls.add(k);
+                map.put(k,map.get(k)-1);
+                per2(nums,ans,ls,map);
                 ls.remove(ls.size()-1);
-                idx.remove(idx.size()-1);
+                map.put(k,map.get(k)+1);
             }
         }
     }
+
+
+    // public void per2(int[] nums,List<List<Integer>> ans,List<Integer> ls,List<Integer> idx){
+    //     if(idx.size()==nums.length){
+    //         if(!ans.contains(ls)){
+    //             ans.add(new ArrayList<>(ls));
+    //         }
+    //         return;
+    //     }
+    //     for(int i=0;i<nums.length;i++){
+    //         if(!idx.contains(i)){
+    //             idx.add(i);
+    //             ls.add(nums[i]);
+    //             per2(nums,ans,ls,idx);
+    //             ls.remove(ls.size()-1);
+    //             idx.remove(idx.size()-1);
+    //         }
+    //     }
+    // }
    
 }
