2585a8007b24a8816663c7b6a60d7dac6d059799
Manasvi gupta <manasvi10amit@gmail.com>
2025-12-13 16:25:12 +0530

    Time: 791 ms (5.04%) | Memory: 46.4 MB (54.95%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> ls=new ArrayList<>();
+        List<Integer> idx=new ArrayList<>();
+        per2(nums,ans,ls,idx);
+        return ans;
+
+        
+    }
+    public void per2(int[] nums,List<List<Integer>> ans,List<Integer> ls,List<Integer> idx){
+        if(idx.size()==nums.length){
+            if(!ans.contains(ls)){
+                ans.add(new ArrayList<>(ls));
+            }
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!idx.contains(i)){
+                idx.add(i);
+                ls.add(nums[i]);
+                per2(nums,ans,ls,idx);
+                ls.remove(ls.size()-1);
+                idx.remove(idx.size()-1);
+            }
+        }
+    }
+    public boolean contain(int[] ar,int in,int e){
+        for(int i=in;i<ar.length;i++){
+            if(ar[i]==e){
+                return true;
+            }
+        }
+        return false;
+    }
+}
