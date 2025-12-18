438062e1dab50d190ca3b4c99a78369701a5b3d9
Manasvi gupta <manasvi10amit@gmail.com>
2025-12-18 23:35:14 +0530

    Time: 2 ms (93.71%) | Memory: 45.9 MB (13.40%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,23 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> ls=new ArrayList<>();
-        comb(ans,ls,candidates,target,0);
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ll= new ArrayList<>();
+        combination(candidates, target,ll, 0,ans);
         return ans;
     }
-    public void comb(List<List<Integer>> ans,List<Integer> ls,int[] ar,int target,int in){
-        if(target==0){
-            ans.add(new ArrayList<>(ls));
+    public static void combination(int [] coin,int amount, List<Integer> ll,int idx,List<List<Integer>> ans){
+        if(amount==0){
+            // System.out.println(ll);
+            ans.add(new ArrayList<>(ll));
             return;
         }
-
-
-        for(int i=in;i<ar.length;i++){
-            if(target>=ar[i]){
-                ls.add(ar[i]);
-                comb(ans,ls,ar,target-ar[i],i);
-                ls.remove(ls.size()-1);
-
+        for(int i=idx;i<coin.length;i++){
+            if(amount>=coin[i]){
+                ll.add(coin[i]);
+                combination(coin,amount-coin[i],ll,i,ans);
+                ll.remove(ll.size()-1); 
             }
         }
     }
