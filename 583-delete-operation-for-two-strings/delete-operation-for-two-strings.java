5ba62c1cfbf48b1c00da820a997896370cd54c91
Manasvi gupta <manasvi10amit@gmail.com>
2025-12-21 09:53:32 +0530

    Time: 8 ms (22.58%) | Memory: 46.7 MB (7.66%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -4,27 +4,27 @@ class Solution {
         for(int i=0;i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        return mindelete(word1,word2,0,0,dp);
+
+        return minsteps(word1,word2,0,0,dp);
+        
     }
-    public int mindelete(String w1,String w2,int i,int j,int[][] dp){
-        if(i==w1.length()){
-            return w2.length()-j;
+    public int minsteps(String s1,String s2,int i,int j,int[][] dp){
+        if(i==s1.length()){
+            return s2.length()-j;
         }
-        if(j==w2.length()){
-            return w1.length()-i;
+        if(j==s2.length()){
+            return s1.length()-i;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int ans=0;
-        if(w1.charAt(i)==w2.charAt(j)){
-            ans=mindelete(w1,w2,i+1,j+1,dp);
+
+        if(s1.charAt(i)==s2.charAt(j)){
+            return dp[i][j]=minsteps(s1,s2,i+1,j+1,dp);
+        }else{
+            int f=minsteps(s1,s2,i+1,j,dp)+1;
+            int se=minsteps(s1,s2,i,j+1,dp)+1;
+            return dp[i][j]=Math.min(f,se);
         }
-        else{
-            int d1=mindelete(w1,w2,i+1,j,dp);
-        int d2=mindelete(w1,w2,i,j+1,dp);
-        ans=Math.min(d1,d2)+1;
-        }
-        return dp[i][j]=ans;
     }
 }
