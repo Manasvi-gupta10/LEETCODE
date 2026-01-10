2859819b5fa929cbef756204ed656bec9b12dba2
Manasvi gupta <manasvi10amit@gmail.com>
2026-01-10 13:58:47 +0530

    Time: 25 ms (26.83%) | Memory: 46.4 MB (58.72%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,34 +1,35 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
         int[][] dp=new int[s1.length()][s2.length()];
-        return minascii(s1,s2,0,0,dp);
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return min(s1,s2,0,0,dp);
     }
-    public int minascii(String s1,String s2,int i,int j,int[][] dp){
+    public int min(String s1,String s2,int i,int j,int[][] dp){
         if(i==s1.length()){
-            int as=0;
-            for(int k=j;k<s2.length();k++){
-                as+=(int)s2.charAt(k);
+            int s=0;
+            for(int in=j;in<s2.length();in++){
+                s+=(int)s2.charAt(in);
             }
-            return as;
+            return s;
         }
         if(j==s2.length()){
-            int as=0;
-            for(int k=i;k<s1.length();k++){
-                as+=(int)s1.charAt(k);
+            int s=0;
+            for(int in=i;in<s1.length();in++){
+                s+=(int)s1.charAt(in);
             }
-            return as;
-        }   
-        if(dp[i][j]!=0){
+            return s;
+        }
+        if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int ans=0;
         if(s1.charAt(i)==s2.charAt(j)){
-            ans=minascii(s1,s2,i+1,j+1,dp);
+            return dp[i][j]=min(s1,s2,i+1,j+1,dp);
         }else{
-            int d1=(int)s1.charAt(i)+minascii(s1,s2,i+1,j,dp);
-            int d2=(int)s2.charAt(j)+minascii(s1,s2,i,j+1,dp);
-            ans=Math.min(d1,d2);
+            int d1=min(s1,s2,i+1,j,dp)+(int)s1.charAt(i);
+            int d2=min(s1,s2,i,j+1,dp)+(int)s2.charAt(j);
+            return dp[i][j]=Math.min(d1,d2);
         }
-        return dp[i][j]=ans;
     }
 }
