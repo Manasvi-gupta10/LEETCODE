4f1f9aeb4ba86f6609b9a76e6c199c53d954f708
Manasvi gupta <manasvi10amit@gmail.com>
2026-01-14 00:41:44 +0530

    Time: 1 ms (99.84%) | Memory: 47.1 MB (33.60%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,11 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int n=nums.length;
-        int xor=0;
+        int ans=0;
         for(int i=0;i<n;i++){
-            xor^=nums[i];
+            ans^=nums[i];
+
         }
-        return xor;
+        return ans;
         
     }
 }
