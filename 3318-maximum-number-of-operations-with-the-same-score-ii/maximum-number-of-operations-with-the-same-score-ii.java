24df59f53bb05263d3ba5fd192184a37f976b423
Manasvi gupta <manasvi10amit@gmail.com>
2026-01-14 23:23:35 +0530

    Time: 1094 ms (27.12%) | Memory: 274.2 MB (62.71%) - LeetSync

---
+++ 3318-maximum-number-of-operations-with-the-same-score-ii/maximum-number-of-operations-with-the-same-score-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    HashMap<String,Integer> map=new HashMap<>();
+    public int maxOperations(int[] nums) {
+        return max(nums,0,nums.length-1,Integer.MIN_VALUE);
+        
+    }
+    public  int max(int[] nums,int st,int end,int prev){
+        if(st>=end){
+            return 0;
+        }
+
+        String key=st+" "+end+" ";
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+
+        int ans=0;
+        int sum1=nums[st]+nums[st+1];
+        if(prev==Integer.MIN_VALUE||sum1==prev){
+            ans=Math.max(ans,1+max(nums,st+2,end,sum1));
+        }
+        int sum2=nums[end]+nums[end-1];
+        if(prev==Integer.MIN_VALUE||sum2==prev){
+            ans=Math.max(ans,1+max(nums,st,end-2,sum2));
+        }
+        int sum3=nums[st]+nums[end];
+        if(prev==Integer.MIN_VALUE||sum3==prev){
+            ans=Math.max(ans,1+max(nums,st+1,end-1,sum3));
+        }
+        map.put(key,ans);
+        return ans;
+    }
+}
