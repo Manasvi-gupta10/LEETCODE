6a95fae781b9610d9b6aadc452b65e4feb24862a
Manasvi gupta <manasvi10amit@gmail.com>
2026-01-20 14:23:14 +0530

    Time: 2766 ms (5.06%) | Memory: 44 MB (80.00%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.java
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -3,25 +3,13 @@ class Solution {
         int n=rating.length;
         int ans=0;
         for(int i=0;i<n;i++){
-            int prevsmall=0,prevlarge=0,nextsmall=0,nextlarge=0;
-            for(int j=0;j<i;j++){
-                if(rating[j]<rating[i]){
-                    prevsmall++;
-                }else if(rating[j]>rating[i]){
-                    prevlarge++;
-                }
-            }
             for(int j=i+1;j<n;j++){
-                 if(rating[j]<rating[i]){
-                    nextsmall++;
-                }else if(rating[j]>rating[i]){
-                    nextlarge++;
+                for(int k=j+1;k<n;k++){
+                    if((rating[i]<rating[j]&&rating[j]<rating[k])||(rating[i]>rating[j]&&rating[j]>rating[k])){
+                        ans++;
+                    }
                 }
             }
-            //calculating us elm k left m small and large elm and same with right 
-            //increasing k liye har prev small map with har next large to make a valid team toh *...same for decreasing
-            ans+=prevsmall*nextlarge;
-            ans+=prevlarge*nextsmall;
         }
         return ans;
     }
