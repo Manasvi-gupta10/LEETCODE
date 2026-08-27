1178e7c31410f0ed1601eb37744cdc326afe1d3e
Manasvi gupta <manasvi.gupta_cs23@gla.ac.in>
2026-08-27 23:02:06 +0530

    Time: 8 ms (6.20%) | Memory: 47.6 MB (6.87%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,37 +1,38 @@
 class Solution {
+    class pair{
+        int v;
+        int cost;
+        public pair(int v,int cost){
+            this.v=v;
+            this.cost=cost;
+        }
+    }
     public boolean isBipartite(int[][] graph) {
-        HashMap<Integer,Integer> visited=new HashMap<>();
+        int n=graph.length;
+        HashMap<Integer,Integer> visit=new HashMap<>();
         Queue<pair> q=new LinkedList<>();
-        for(int i=0;i<graph.length;i++){
-            if(visited.containsKey(i)){
+        for(int i=0;i<n;i++){
+            if(visit.containsKey(i)){
                 continue;
             }
             q.add(new pair(i,0));
-            while(!q.isEmpty()){
-                pair r=q.poll();
-                if(visited.containsKey(r.vtx)){
-                    if(visited.get(r.vtx)!=r.dis){
-                        return false;
-                    }else{
-                        continue;
-                    }
+        while(!q.isEmpty()){
+            pair rp=q.poll();
+            if(visit.containsKey(rp.v)){
+                if(rp.cost!=visit.get(rp.v)){
+                    return false;
                 }
-                visited.put(r.vtx,r.dis);
-                for(int nbr:graph[r.vtx]){
-                    if(!visited.containsKey(nbr)){
-                        q.add(new pair(nbr,r.dis+1));
-                    }
+                continue;
+            }
+            visit.put(rp.v,rp.cost);
+            for(int nb:graph[rp.v]){
+                if(!visit.containsKey(nb)){
+                    q.add(new pair(nb,rp.cost+1));
                 }
             }
         }
+        }
         return true;
-    }
-}
-class pair{
-    int vtx;
-    int dis;
-    public pair(int vtx,int dis){
-        this.vtx=vtx;
-        this.dis=dis;
+
     }
 }
