2d90ccebe991b3a5fbc9a76c560eded55da173a7
Manasvi gupta <manasvi.gupta_cs23@gla.ac.in>
2026-09-09 11:12:38 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (61.21%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,20 +1,22 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean[][] visit=new boolean[image.length][image[0].length];
-        dfs(image,sr,sc,color,image[sr][sc],visit);
+        int prev=image[sr][sc];
+        if (prev == color) return image;
+        int r=image.length;
+        int c=image[0].length;
+        fill(image,sr,sc,color,image[sr][sc]);
         return image;
+
     }
-    public void dfs(int[][] grid,int sr,int sc,int color,int prevcolor,boolean[][] visit){
-        if(sr<0||sc<0||sr>=grid.length||sc>=grid[0].length||grid[sr][sc]!=prevcolor||visit[sr][sc]==true){
+    public void fill(int[][] ar,int r,int c,int col,int prev){
+        if(r<0||c<0||r>=ar.length||c>=ar[0].length||ar[r][c]!=prev){
             return;
         }
-        visit[sr][sc]=true;
-        grid[sr][sc]=color;
-        dfs(grid,sr+1,sc,color,prevcolor,visit);
-        dfs(grid,sr,sc+1,color,prevcolor,visit);
-        dfs(grid,sr-1,sc,color,prevcolor,visit);
-        dfs(grid,sr,sc-1,color,prevcolor,visit);
-
-
+        ar[r][c]=col;
+        fill(ar,r+1,c,col,prev);
+        fill(ar,r,c+1,col,prev);
+        fill(ar,r-1,c,col,prev);
+        
+        fill(ar,r,c-1,col,prev);
     }
 }
