83234091e1db819953e46fd5455cd4d7cbe2bcf8
Manasvi gupta <manasvi.gupta_cs23@gla.ac.in>
2026-09-09 11:35:06 +0530

    Time: 60 ms (5.00%) | Memory: 49.9 MB (8.68%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,51 +1,49 @@
 class Solution {
+    class pair{
+        int r;
+        int c;
+        int cost;
+        public pair(int r,int c,int cost){
+            this.r=r;
+            this.c=c;
+            this.cost=cost;
+        }
+    }
     public int[][] updateMatrix(int[][] mat) {
         int r=mat.length;
         int c=mat[0].length;
-        int[][] ans=new int[r][c];
-        int[] row={0,0,1,-1};
-        int[] col={-1,1,0,0};
-        Queue<pair> q=new LinkedList<>();
-
-        for(int i=0;i<r;i++){
+        PriorityQueue<pair> pq=new PriorityQueue<>((a,b)->a.cost-b.cost);
+        boolean[][] visit=new boolean[r][c];
+         int[][] ans=new int[r][c];
+         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(mat[i][j]==0){
-                    q.add(new pair(i,j));
-
-                }else{
-                    ans[i][j]=Integer.MAX_VALUE;
+                    pq.add(new pair(i,j,0));
                 }
             }
-        }
-        while(!q.isEmpty()){
-            pair rp=q.poll();
-
-            for(int i=0;i<4;i++){
-                int newr=rp.r+row[i];
-                int newc=rp.c+col[i];
-                if(newr<0||newc<0||newr>=r||newc>=c){
+         }
+         int[] row={1,-1,0,0};
+         int[] col={0,0,1,-1};
+         while(!pq.isEmpty()){
+            pair p=pq.poll();
+            int i=p.r;
+            int j=p.c;
+            if(visit[i][j]){
+                continue;
+            }
+            visit[i][j]=true;
+            if(mat[i][j]==1){
+                ans[i][j]=p.cost;
+            }
+            for(int k=0;k<4;k++){
+                int ni=i+row[k];
+                int nj=j+col[k];
+                if(ni<0||nj<0||ni>=r||nj>=c||visit[ni][nj]){
                     continue;
                 }
-                if(ans[newr][newc]>ans[rp.r][rp.c]+1){
-                    ans[newr][newc]=ans[rp.r][rp.c]+1;
-                    q.add(new pair(newr,newc));
-                }
+                pq.add(new pair(ni,nj,p.cost+1));
             }
-            
-        }
-        return ans;
-    }
-    
-}
-class pair{
-   
-    int r;
-    int c;
-    
-    public pair(int r,int c){
-       
-        this.r=r;
-        this.c=c;
-        
+         }
+         return ans;
     }
 }
